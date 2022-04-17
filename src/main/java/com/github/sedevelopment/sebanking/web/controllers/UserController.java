0c4f1bd2c3ad43c7fec3b72e8a153c46a5d9c043
Samuel Echols <samuel.echols@selu.edu>
2022-04-17 15:58:18 -0500

    created role entitty

--- src/main/java/com/github/sedevelopment/sebanking/web/controllers/UserController.java
+++ src/main/java/com/github/sedevelopment/sebanking/web/controllers/UserController.java
@@ -1,12 +1,11 @@
 package com.github.sedevelopment.sebanking.controllers;
 
 import com.github.sedevelopment.sebanking.models.user.User;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 
 @RestController
 public class UserController {
